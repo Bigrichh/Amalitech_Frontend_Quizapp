18f618c6a954aa404a4c31378499348b1e4c8231
Richmann <richmanaddai@gmail.com>
2024-07-17 15:30:38 +0000

    persisiting theme to local storage

--- src/stores/quizstore.ts
+++ src/stores/quizstore.ts
@@ -4,8 +4,8 @@ import data from "../../data.json"
 export const useQuizStore = defineStore('quiz', {
   state: () => ({
     subjects: [] as any[],
-    toggled: false,
-    theme: 'light-mode',
+    toggled: JSON.parse(localStorage.getItem('toggled') || 'false'),
+    theme: localStorage.getItem('theme') || 'light-mode',
   }),
 
   actions: {
@@ -16,6 +16,8 @@ export const useQuizStore = defineStore('quiz', {
     toggleTheme() {
       this.toggled = !this.toggled;
       this.theme = this.toggled ? 'dark-mode' : 'light-mode';
+      localStorage.setItem('theme', this.theme); // Save theme to local storage
+      localStorage.setItem('toggled', this.toggled); // Save theme to local storage
     },
   },
 
